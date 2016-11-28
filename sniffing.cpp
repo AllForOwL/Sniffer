0a49766fff3496b2b1ed0d80f5e626292e22c35d
root <root@localhost.localdomain>
2016-11-28 00:06:58 -0700

    Add read Stream

--- sniffing.cpp
+++ sniffing.cpp
@@ -1,40 +1,88 @@
 #include "sniffing.h"
 #include <vector>
-#include <tins/tins.h>
 #include <fstream>
 #include <QFile>
 #include <QMessageBox>
 #include <QTextStream>
 #include <QThread>
 
-using namespace Tins;
+const size_t MAX_PAYLOAD = 3 * 1024;
+
+std::vector<QString> Sniffing::m_vecClientData;
+std::vector<QString> Sniffing::m_vecServerData;
 
 Sniffing::Sniffing()
 {
 
 }
 
+void Sniffing::on_server_data(Stream& i_stream)
+{
+    const Stream::payload_type& _client_payload = i_stream.client_payload();
+    const Stream::payload_type& _server_payload = i_stream.server_payload();
+
+    m_vecClientData.clear();
+    m_vecServerData.clear();
+
+    for (int i = 0; i < _client_payload.size(); i++)
+    {
+        m_vecClientData.push_back(QString(_client_payload[i]));
+    }
+    for (int i = 0; i < _server_payload.size(); i++)
+    {
+        m_vecServerData.push_back(QString(_server_payload[i]));
+    }
+
+    if (i_stream.server_payload().size() > MAX_PAYLOAD)
+    {
+        i_stream.ignore_server_data();
+    }
+}
+
+void Sniffing::on_client_data(Stream& i_stream)
+{
+    if (i_stream.client_payload().size() > MAX_PAYLOAD)
+    {
+        i_stream.ignore_client_data();
+    }
+}
+
+void Sniffing::on_new_connection(Stream& i_stream)
+{
+    i_stream.client_data_callback(&on_client_data);
+    i_stream.server_data_callback(&on_server_data);
+
+    i_stream.auto_cleanup_payloads(false);
+}
+
 void Sniffing::StartSniffing()
 {
     Packet _packet;
     Sniffer sniffer("eth0");
+    StreamFollower _tcpStream;
 
     while(true)
     {
         _packet  = sniffer.next_packet();
 
-        if (_packet.pdu()->find_pdu<IP>())
+        if (_packet.pdu()->find_pdu<TCP>())
         {
+            _tcpStream.new_stream_callback(&on_new_connection);
+            _tcpStream.process_packet(_packet);
+
+            const IP& _ip                   = _packet.pdu()->rfind_pdu<IP>();
+            const TCP& _tcp                 = _packet.pdu()->rfind_pdu<TCP>();
+
             QFile file("/tmp/packet.txt");
 
              std::ofstream _writeIP("/tmp/packet.txt");
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().id()                 << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().frag_off()      << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().src_addr()           << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().dst_addr()           << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().protocol()  << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().size()               << std::endl;
-             _writeIP <<_packet.pdu()->rfind_pdu<IP>().fragment_offset()   << std::endl;
+             _writeIP <<_ip.id()        << std::endl;
+             _writeIP <<_ip.src_addr()  << std::endl;
+             _writeIP <<_ip.dst_addr()  << std::endl;
+             _writeIP <<_ip.protocol()  << std::endl;
+             _writeIP <<_tcp.size()     << std::endl;
+             _writeIP <<_tcp.sport()    << std::endl;
+             _writeIP <<_tcp.dport()    << std::endl;
 
 
             _writeIP.close();
--- sniffing.h
+++ sniffing.h
@@ -3,12 +3,26 @@
 
 #include <QObject>
 #include <QWidget>
+#include <tins/tins.h>
+#include <tins/tcp_ip/stream_follower.h>
+#include <tins/sniffer.h>
+
+using namespace Tins;
+using Tins::Packet;
+using Tins::Sniffer;
+using Tins::SnifferConfiguration;
+using Tins::TCPIP::Stream;
+using Tins::TCPIP::StreamFollower;
+
 
 class Sniffing : public QObject
 {
     Q_OBJECT
 public:
     Sniffing();
+    static void on_server_data(Stream& i_stream);
+    static void on_client_data(Stream& i_stream);
+    static void on_new_connection(Stream& i_stream);
 
 signals:
     void CompleteReadPacket(QString i_id,
@@ -18,8 +32,15 @@ signals:
                             QString i_protocol,
                             QString i_length,
                             QString i_info);
+
+    void ReadClientData(QString i_data);
+    void ReadServerData(QString i_data);
 public slots:
     void StartSniffing();
+
+private:
+    static std::vector<QString> m_vecClientData;
+    static std::vector<QString> m_vecServerData;
 };
 
 #endif // SNIFFING_H
